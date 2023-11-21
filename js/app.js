27ad092d0ff8eee4e149cfa3789a6dd48b74400a
SergioChica <sergioanjaci@gmail.com>
2023-11-21 02:54:23 -0500

    Adding menu structure and layout along with media query

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
-const menu = document.querySelector('#catalog');
+const buttonMenu = document.querySelector('#catalog');
 
 function convertMenu() {
   if (window.innerWidth <= 480 ) {
-    menu.innerHTML = '<span class="material-symbols-outlined">menu</span>';
+    buttonMenu.innerHTML = '<span class="material-symbols-outlined">menu</span>';
   }else{
-    menu.textContent = 'Catalogo';
+    buttonMenu.textContent = 'Catalogo';
   }
 }
 
 window.addEventListener("load", convertMenu);
 window.addEventListener("resize", convertMenu);
+
+const menu = document.querySelector('#menuCatalog')
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  buttonMenu.addEventListener('click', openMenu);
+
+  function openMenu() {
+    if (menu.computedStyleMap.display==='none') {
+      menu.computedStyleMap.display='flex';
+    }else{
+      menu.computedStyleMap.display='none';
+    }
+  }
+
+  document.addEventListener('click', closeMenu(event));
+
+  function closeMenu(event) {
+    if (!menu.contains(event.target) && event.target !== buttonMenu) {
+      menu.computedStyleMap.display= 'none';
+    }
+  }
+}) 
