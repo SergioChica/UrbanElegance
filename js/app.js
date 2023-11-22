82ed214d82fb92505cc36e21e06d4b5b7d42e269
SergioChica <sergioanjaci@gmail.com>
2023-11-21 21:39:43 -0500

    Comments in js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const iconMenu = document.querySelector('#catalog');
 const containerMenu = document.querySelector('#menu');
 
+//Function to convert the menu
 function convertMenu() {
-  if (window.innerWidth <= 480 ) {
+  //If the width is less than 480px we change iconMenu to hamburger menu icon and change the class of the menu div, otherwise we leave the icon menu normal and do not change the class.
+  if (window.innerWidth <= 480) {
     iconMenu.innerHTML = '<span class="material-symbols-outlined" id="buttonMenu">menu</span>';
     containerMenu.className='menuPhone';
   }else{
@@ -10,34 +12,43 @@ function convertMenu() {
   }
 }
 
+//Activate when the window loads.
 window.addEventListener("load", convertMenu);
+//Activate when the window size is changed.
 window.addEventListener("resize", convertMenu);
 
-
-window.addEventListener("load", function () {
+//Function to activate menu actions.
+function actionsMenu() {
+    const buttonMenu = document.querySelector("#buttonMenu");
+    const menu = document.querySelector('#menuCatalog')
+    const body = document.body;
   
-  const buttonMenu = document.querySelector("#buttonMenu");
-  const menu = document.querySelector('#menuCatalog')
-  const body = document.body;
-
-  buttonMenu.addEventListener('click', openMenu);
-  
-  function openMenu() {
-    if (getComputedStyle(menu).display ==='none') {
-      menu.style.display='flex';
-      body.style.overflow='hidden';
-    }else{
-      menu.syle.display='none';
+    //Event to open the menu.
+    buttonMenu.addEventListener('click', openMenu);
+    
+    //Function to open menu.
+    function openMenu() {
+      //If menu has a display in none we give a display flex to make visible the menu and we apply an overflow hidden to the body to remove the scroll to the page, if not we leave in display none and we do not remove scroll of the page.
+      if (getComputedStyle(menu).display ==='none') {
+        menu.style.display='flex';
+        body.style.overflow='hidden';
+      }else{
+        menu.syle.display='none';
+      }
+    }
+    
+    //Event to close the menu.
+    document.addEventListener('click', closeMenu);
+    
+    //Function to close the menu
+    function closeMenu(event) {
+      //If we click on a place outside the menu we give display none to the menu to close the menu and we give overflow auto to the body so that it is possible to make scroll in the page.
+      if (!menu.contains(event.target) && event.target !== buttonMenu) {
+        menu.style.display= 'none';
+        body.style.overflow='auto';
+      }
     }
   }
 
-  document.addEventListener('click', closeMenu);
-
-  function closeMenu(event) {
-    if (!menu.contains(event.target) && event.target !== buttonMenu) {
-      menu.style.display= 'none';
-      body.style.overflow='auto';
-    }
-  }
-
-});
+//Activate when the window is loads.
+window.addEventListener("load", actionsMenu) 
